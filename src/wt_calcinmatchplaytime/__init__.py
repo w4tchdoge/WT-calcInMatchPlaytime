82e3a7be89f659701c61fd342a487e603c594fae
w4tchdoge <w4tchdoge@protonmail.com>
2025-06-24 19:21:25 +0800

    cleanup input arguements' helptext
    remove the last full stop in the help description of `--filename` to make it
    consistent with the help descriptions of the other arguements

--- src/wt_calcinmatchplaytime/__init__.py
+++ src/wt_calcinmatchplaytime/__init__.py
@@ -5,7 +5,7 @@ from wt_calcinmatchplaytime import calc
 
 def main() -> None:
 	parser = argparse.ArgumentParser(description="A Python CLI application that takes a TSV (or CSV) file containing your in-game match time and outputs the total playtime in hours.")
-	parser.add_argument("-i", "--filename", required=True, help="TSV (or CSV) file that contains the in-match playtime. col 1 of the file is vehicle type and col 2 is time played.")
+	parser.add_argument("-i", "--filename", required=True, help="TSV (or CSV) file that contains the in-match playtime. col 1 of the file is vehicle type and col 2 is time played")
 	parser.add_argument("-nh", "--no-headers", help="Indicate that the input file does not have headers", action="store_true")
 	args = parser.parse_args()
 	# print(args)
